cf4761d0b4cfeed74a22771350936fc3b0048f1f
Vivek Sahu <viveksahu23@gmail.com>
2024-01-02 21:01:22 +0530

    naming typo

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,9 +2,9 @@
 
 import { useState, useEffect } from "react";
 
-import { CreateServerModal } from "@/components/modals/create-server";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
-import { EditServerModal } from "@/components/modals/edit-server";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 
 export const ModalProvider = () => {
