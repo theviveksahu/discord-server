4bc50dbd7ab7dc15567c9da2d5552d4b000f9353
Vivek Sahu <viveksahu23@gmail.com>
2024-01-05 22:01:58 +0530

    socket io setup

--- types.ts
+++ types.ts
@@ -1,5 +1,13 @@
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
+
 import { Server, Profile, Member } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & { server: NetServer & { io: SocketIOServer } };
 };
