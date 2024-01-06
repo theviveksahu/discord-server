6bd4bc8db2e88ece6a08e821809d8073524c88ca
Vivek Sahu <viveksahu23@gmail.com>
2024-01-06 14:25:00 +0530

    file upload

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,13 +2,13 @@
 
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: "messagefile" | "serverImage";
+  endpoint: "messageFile" | "serverImage";
 }
 
 export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
@@ -28,10 +28,33 @@ export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
       </div>
     );
   }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
   return (
     <div>
       <UploadDropzone
-        endpoint="serverImage"
+        endpoint={endpoint}
         onClientUploadComplete={(res) => {
           onChange(res?.[0].url);
         }}
